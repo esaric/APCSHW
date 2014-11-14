f4ee38b833c67dc7fad17b552b32adebcd3943c2
Elias Saric <eliassaric@gmail.com>
2014-11-13 23:42:16 -0500

    finished collapseDulplicates

--- 02ArrayList/ArrayListMethods.java
+++ 02ArrayList/ArrayListMethods.java
@@ -7,13 +7,33 @@ public class ArrayListMethods{
 		for (int i = 0; i < 10; i++) {
 			list.add(i);
 		}
-		System.out.println(list);
+		/*System.out.println(list);
 		ArrayListMethods.randomize(list);
+		System.out.println(list);
+		*/
+		list.add(9);
+		list.add(9);
+		list.add(9);
+		ArrayListMethods.collapseDuplicates(list);
 		System.out.println(list);
     }
 	
 	public static void collapseDuplicates(ArrayList<Integer> L){
-		
+		ArrayList<Integer> copy = new ArrayList<Integer>(L.size());
+		copy.add(L.get(0));
+		for (int i = 1; i < L.size(); i++){
+			if (L.get(i) != L.get(i-1)) {
+				copy.add(L.get(i));
+			}
+			//System.out.println(copy);
+		}
+		for (int i = 0; i < copy.size(); i++){
+			L.set(i, copy.get(i));
+		}
+		for (int i = L.size() - copy.size(); i > 0; i--){
+			//System.out.println(i);
+			L.remove(L.size() - i);
+		}
 	}
 	
 	public static void randomize(ArrayList<Integer> L) {
