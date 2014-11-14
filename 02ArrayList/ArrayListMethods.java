05e72c7495c1fa01d693faaeb3d09ec08da27dd9
Elias Saric <eliassaric@gmail.com>
2014-11-13 22:53:41 -0500

    folder test

---
+++ 02ArrayList/ArrayListMethods.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ArrayListMethods{
+	public static void collapseDuplicates(ArrayList<Integer> L){
+		
+	}
+	
+	public static void randomize(ArrayList<Integer> L) {
+		
+    }
+}
