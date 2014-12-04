7946149f934f9fca710822676bc75b574f14d64d
esaric <eliassaric@gmail.com>
2014-12-03 21:01:57 -0500

    Lazy solutions to all problems

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,3 +1,24 @@
 public class OrderedSuperArray extends SuperArray{
 
+	public void add(int index, String o){
+        add(o);
+    }
+	
+	public void add(String o){
+		for (int i = size(); i > 0 && get(i - 1).compareTo(o) > 0; i--);
+		super.add(i, o);
+	}
+	
+	/*public void add(String o){
+        int index = size();
+        while( index >0 && get(index-1).compareTo(o) > 0 ){
+            index--;
+        }
+        super.add(index, o);
+    }*/
+	
+	public String set(int index, String o){
+        String removed = remove(index);
+        add(o);
+        return removed;
 }
